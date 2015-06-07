959e3133bb8d493a07da7755927d2544c547e4eb
victorkzk <victor_kzk@mail.ru>
2015-06-07 07:53:24 +0300

    Created client project

---
+++ client/src/application/Controller.java
@@ -0,0 +1,17 @@
+package application;
+
+import com.sun.javafx.geom.Rectangle;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void squareMouseEntered() {
+        System.out.println();
+    }
+
+    @FXML
+    private void squareMouseExited() {
+
+    }
+}
---
+++ client/src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("View.fxml"));
+        Scene scene = new Scene(loader.<Parent>load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
